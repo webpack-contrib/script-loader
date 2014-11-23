48db56fd7f9167fde88f79a05794bfb21466bd58
eternalko <eternalko@gmail.com>
2014-11-23 02:00:09 +0100

    Make script loader Web Worker friendly
    Since `window` is not accessible in a Web Worker context, code like:
    `require("script!myModule.js”);` will not work

--- addScript.js
+++ addScript.js
@@ -3,8 +3,20 @@
 	Author Tobias Koppers @sokra
 */
 module.exports = function(src) {
-	if (window.execScript)
-		window.execScript(src);
-	else
-		eval.call(null, src);
+
+	// Assume that we use a normal browsing mode
+	if(typeof window !== "undefined") {
+		if (window.execScript)
+			window.execScript(src);
+		else
+			eval.call(null, src);
+	}
+	
+	// Assume that we load a script in a web worker
+	if(typeof self !== "undefined") {
+		if (self.execScript)
+			self.execScript(src);
+		else
+			eval.call(null, src);
+	}
 }
