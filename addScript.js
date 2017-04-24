3fc96976c251e0bf151261855fc896ddc4d78ffe
Dan Shearmur <dan.shearmur@gmail.com>
2017-04-24 14:56:30 +0100

    use a script tag instead of eval

--- addScript.js
+++ addScript.js
@@ -2,8 +2,19 @@
 	MIT License http://www.opensource.org/licenses/mit-license.php
 	Author Tobias Koppers @sokra
 */
+var firstScript;
+
+function inlineScript(src) {
+	var script = document.createElement('script');
+	script.innerHTML = src;
+	firstScript = firstScript || document.getElementsByTagName('script')[0];
+	firstScript.parentNode.insertBefore(script, firstScript);
+}
+
 module.exports = function(src) {
-	if (typeof execScript !== "undefined")
+	if (typeof document !== 'undefined')
+		inlineScript(src);
+	else if (typeof execScript !== "undefined")
 		execScript(src);
 	else
 		eval.call(null, src);
