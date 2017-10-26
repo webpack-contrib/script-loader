8010e4222af392f4b65ac90f22d0a42e32c70c4a
Jim Edelstein <jim.edelstein@gmail.com>
2017-10-26 15:31:53 -0400

    Fix issue with absolute paths using loaderUtils.stringifyRequest instead of JSON.stringify

--- index.js
+++ index.js
@@ -3,18 +3,19 @@
 	Author Tobias Koppers @sokra
 */
 var path = require("path");
+var loaderUtils = require("loader-utils");
 module.exports = function() {};
 module.exports.pitch = function(remainingRequest) {
-	this.cacheable && this.cacheable();
-	return "require(" + JSON.stringify("!!" + path.join(__dirname, "addScript.js")) + ")"+
-			"(require(" +
-			JSON.stringify("!!" + require.resolve("raw-loader") + "!" + remainingRequest) + ")" +
-				(this.debug ?
-					"+" +
-						JSON.stringify(
-							"\n\n// SCRIPT-LOADER FOOTER\n//# sourceURL=script:///" +
-								encodeURI(remainingRequest.replace(/^!/, "")).replace(/%5C|%2F/g, "/").replace(/\?/, "%3F").replace(/^\//, "")
-						) :
-					"") +
-			")";
-};
+  this.cacheable && this.cacheable();
+  return "require(" + loaderUtils.stringifyRequest(this, "!!" + path.join(__dirname, "addScript.js")) + ")"+
+    "(require(" +
+    loaderUtils.stringifyRequest(this, "!!" + require.resolve("raw-loader") + "!" + remainingRequest) + ")" +
+    (this.debug ?
+      "+" +
+      JSON.stringify(
+        "\n\n// SCRIPT-LOADER FOOTER\n//# sourceURL=script:///" +
+        encodeURI(remainingRequest.replace(/^!/, "")).replace(/%5C|%2F/g, "/").replace(/\?/, "%3F").replace(/^\//, "")
+      ) :
+      "") +
+    ")";
+};	
