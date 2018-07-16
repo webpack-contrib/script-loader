f692c33554a796f6dffbf3f8d845a6f44a663a56
Timothy Purchas <s108647@Timothys-MacBook-Pro.local>
2018-07-16 19:08:09 +1000

    Use Strict Fix

--- addScript.js
+++ addScript.js
@@ -2,10 +2,10 @@
 	MIT License http://www.opensource.org/licenses/mit-license.php
 	Author Tobias Koppers @sokra
 */
-module.exports = function(src) {
+module.exports = function (src) {
 	function log(error) {
 		(typeof console !== "undefined")
-		&& (console.error || console.log)("[Script Loader]", error);
+			&& (console.error || console.log)("[Script Loader]", error);
 	}
 
 	// Check for IE =< 8
@@ -14,6 +14,8 @@ module.exports = function(src) {
 	}
 
 	try {
+		src = src.replace(/('|")use strict('|");?/, '');
+
 		if (typeof execScript !== "undefined" && isIE()) {
 			execScript(src);
 		} else if (typeof eval !== "undefined") {
